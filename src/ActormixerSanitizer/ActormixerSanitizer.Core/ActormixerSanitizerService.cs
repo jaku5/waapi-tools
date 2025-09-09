a980fdb2824d256c69928e7f218325c40e88017d
jakubp <jakubp@jpaudio.org>
2025-09-09 00:21:49 -0700

    Reintroduce ActorMixerInfo class and remove from service

--- src/ActormixerSanitizer/ActormixerSanitizer.Core/ActormixerSanitizerService.cs
+++ src/ActormixerSanitizer/ActormixerSanitizer.Core/ActormixerSanitizerService.cs
@@ -271,16 +271,4 @@ namespace JPAudio.WaapiTools.Tool.ActormixerSanitizer.Core
             return await client.Call(ObjectGetUri, query, options);
         }
     }
-
-    public class ActorMixerInfo
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Path { get; set; }
-        public string ParentId { get; set; }
-        public string Notes { get; set; }
-        public string AncestorId { get; set; }
-        public string AncestorName { get; set; }
-        public bool IsSelected { get; set; }
-    }
 }
--- src/ActormixerSanitizer/ActormixerSanitizer.Core/Models/ActorMixerInfo.cs
+++ src/ActormixerSanitizer/ActormixerSanitizer.Core/Models/ActorMixerInfo.cs
@@ -1,10 +1,11 @@
-//public class ActorMixerInfo
-//{
-//    public string Id { get; set; }
-//    public string Name { get; set; }
-//    public string Path { get; set; }
-//    public string ParentId { get; set; }
-//    public string Notes { get; set; }
-//    public string AncestorId { get; set; }
-//    public string AncestorName { get; set; }
-//}
+public class ActorMixerInfo
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Path { get; set; }
+    public string ParentId { get; set; }
+    public string Notes { get; set; }
+    public string AncestorId { get; set; }
+    public string AncestorName { get; set; }
+    public bool IsSelected { get; set; }
+}
