750cd9789ce12a18a9737154519d4d9b3f314b21
jakubp <jakubp@jpaudio.org>
2025-09-17 10:37:22 +0200

    Add double click item feature

--- src/ActormixerSanitizer/ActormixerSanitizer.UI/MainWindow.xaml.cs
+++ src/ActormixerSanitizer/ActormixerSanitizer.UI/MainWindow.xaml.cs
@@ -20,5 +20,14 @@ namespace ActormixerSanitizer.UI
         {
             InitializeComponent();
         }
+
+        private void ListBoxItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is ListBoxItem item && item.DataContext is ActorMixerInfo actor)
+            {
+                actor.IsSelected = !actor.IsSelected;
+                e.Handled = true;
+            }
+        }
     }
 }
